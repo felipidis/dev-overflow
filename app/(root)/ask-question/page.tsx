86c6b515944462b60a699f52a471aa4455a46160
Felipe Pimentel <felipekdra@gmail.com>
2023-10-09 16:08:40 -0300

    feat: get user info with a mocked user (temp)

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,21 @@
-const AskQuestion = () => {
-  return <div>Ask Question</div>
+import QuestionForm from '@/components/forms/QuestionForm'
+import { getUserById } from '@/lib/actions/user.action'
+import { redirect } from 'next/navigation'
+const AskQuestion = async () => {
+  // const { userId } = auth()
+  const userId = '123456789'
+  if (!userId) redirect('/sign-in')
+
+  const mongoUser = await getUserById({ userId })
+
+  return (
+    <div>
+      <h1 className='h1-bold text-dark100_light900'> Ask a question</h1>
+      <div className='mt-9'>
+        <QuestionForm mongoUserId={JSON.stringify(mongoUser._id)} />
+      </div>
+    </div>
+  )
 }
 
 export default AskQuestion
