9a4da4aca8ed3e5dcd38303b6ef42e3c9e999ea3
Felipe Pimentel <felipekdra@gmail.com>
2023-10-17 09:28:49 -0300

    feat: add url query handlers

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,5 @@
 import { clsx, type ClassValue } from 'clsx'
+import qs from 'query-string'
 import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
@@ -46,4 +47,43 @@ export const getJoinedDate = (date: Date): string => {
   const joinedDate = `${month} ${year}`
 
   return joinedDate
+}
+
+type UrlQueryParams = {
+  params: string
+  key: string
+  value: string | null
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const currentUrl = qs.parse(params)
+
+  currentUrl[key] = value
+
+  return qs.stringifyUrl(
+    { url: window.location.pathname, query: currentUrl },
+    {
+      skipNull: true
+    }
+  )
+}
+
+type RemoveUrlQueryParams = {
+  params: string
+  keys: string[]
+}
+
+export const removeKeysFromQuery = ({ params, keys }: RemoveUrlQueryParams) => {
+  const currentUrl = qs.parse(params)
+
+  keys.forEach((key) => {
+    delete currentUrl[key]
+  })
+
+  return qs.stringifyUrl(
+    { url: window.location.pathname, query: currentUrl },
+    {
+      skipNull: true
+    }
+  )
 }
