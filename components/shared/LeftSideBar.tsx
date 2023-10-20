e92fa8e128a5c7cb6ae6a1736e8d0bfbfbf15479
Felipe Pimentel <felipekdra@gmail.com>
2023-10-20 19:14:45 -0300

    fix: change html tags order to fix hydration

--- components/shared/LeftSideBar.tsx
+++ components/shared/LeftSideBar.tsx
@@ -40,7 +40,7 @@ const LeftSideBar = () => {
                 alt={item.label}
                 width={20}
                 height={20}
-                className={` ${!isActive && 'invert-colors'}`}
+                className={` ${!isActive ? 'invert-colors' : ''}`}
               />
               <p
                 className={`${
@@ -75,7 +75,7 @@ const LeftSideBar = () => {
             <Button className='small-medium light-border-2 btn-tertiary text-dark400_light900 min-h-[41px] w-full rounded-lg px-4 py-3 shadow-none'>
               <Image
                 src='/assets/icons/sign-up.svg'
-                alt='login'
+                alt='signUp'
                 width={20}
                 height={20}
                 className='invert-colors lg:hidden'
